4beae57a6bb930b86d62d5c0ac2c59b7361bf10e
khardigree <kaitlynnhardigree@gmail.com>
2024-03-28 18:14:57 -0400

    made more family friendly

--- pm.js
+++ pm.js
@@ -2,7 +2,7 @@ let answer = prompt("What is your favorite Pokemon?")
 
 
 if (answer === "Mega Rayquaza") {
-    alert ("Fuck yeah, you're correct!");
+    alert ("YEEAAHHH, you're correct!");
 } else {
     alert ("BOOOOOOOOOOOOO")
 }
